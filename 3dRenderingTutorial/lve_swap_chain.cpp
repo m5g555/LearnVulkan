edfc075d25d085f5306a819a532801efc5f4e848
m5g555 <mitchelg555@gmail.com>
2025-12-19 06:50:49 -0500

    Fixed incorrect code handling semaphores for presenting image.
    See: https://docs.vulkan.org/guide/latest/swapchain_semaphore_reuse.html

--- 3dRenderingTutorial/lve_swap_chain.cpp
+++ 3dRenderingTutorial/lve_swap_chain.cpp
@@ -64,10 +64,13 @@ LveSwapChain::~LveSwapChain() {
     // cleanup synchronization objects
     for (size_t i = 0; i < MAX_FRAMES_IN_FLIGHT; i++) {
         vkDestroySemaphore(
-            device.device(), renderFinishedSemaphores[i], nullptr);
-        vkDestroySemaphore(
             device.device(), imageAvailableSemaphores[i], nullptr);
         vkDestroyFence(device.device(), inFlightFences[i], nullptr);
+    }
+
+    for (size_t i = 0; i < imageCount(); i++) {
+        vkDestroySemaphore(
+            device.device(), renderFinishedSemaphores[i], nullptr);
     }
 }
 
@@ -114,7 +117,7 @@ VkResult LveSwapChain::submitCommandBuffers(const VkCommandBuffer* buffers,
     submitInfo.commandBufferCount = 1;
     submitInfo.pCommandBuffers = buffers;
 
-    VkSemaphore signalSemaphores[] = {renderFinishedSemaphores[currentFrame]};
+    VkSemaphore signalSemaphores[] = {renderFinishedSemaphores[*imageIndex]};
     submitInfo.signalSemaphoreCount = 1;
     submitInfo.pSignalSemaphores = signalSemaphores;
 
@@ -206,6 +209,7 @@ void LveSwapChain::createSwapChain() {
     // again to retrieve the handles.
     vkGetSwapchainImagesKHR(device.device(), swapChain, &imageCount, nullptr);
     swapChainImages.resize(imageCount);
+
     vkGetSwapchainImagesKHR(
         device.device(), swapChain, &imageCount, swapChainImages.data());
 
@@ -379,7 +383,7 @@ void LveSwapChain::createDepthResources() {
 
 void LveSwapChain::createSyncObjects() {
     imageAvailableSemaphores.resize(MAX_FRAMES_IN_FLIGHT);
-    renderFinishedSemaphores.resize(MAX_FRAMES_IN_FLIGHT);
+    renderFinishedSemaphores.resize(imageCount());
     inFlightFences.resize(MAX_FRAMES_IN_FLIGHT);
     imagesInFlight.resize(imageCount(), VK_NULL_HANDLE);
 
@@ -390,15 +394,20 @@ void LveSwapChain::createSyncObjects() {
     fenceInfo.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
     fenceInfo.flags = VK_FENCE_CREATE_SIGNALED_BIT;
 
+    for (size_t i = 0; i < imageCount(); i++) {
+        if (vkCreateSemaphore(device.device(),
+                              &semaphoreInfo,
+                              nullptr,
+                              &renderFinishedSemaphores[i]) != VK_SUCCESS) {
+            throw std::runtime_error(
+                "Failed to create render finished semaphores for a frame");
+        }
+    }
     for (size_t i = 0; i < MAX_FRAMES_IN_FLIGHT; i++) {
         if (vkCreateSemaphore(device.device(),
                               &semaphoreInfo,
                               nullptr,
                               &imageAvailableSemaphores[i]) != VK_SUCCESS ||
-            vkCreateSemaphore(device.device(),
-                              &semaphoreInfo,
-                              nullptr,
-                              &renderFinishedSemaphores[i]) != VK_SUCCESS ||
             vkCreateFence(
                 device.device(), &fenceInfo, nullptr, &inFlightFences[i]) !=
                 VK_SUCCESS) {
