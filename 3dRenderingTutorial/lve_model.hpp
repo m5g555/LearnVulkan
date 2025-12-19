b574f7b87a18b43c37a31189edf5d618a65ef7f2
m5g555 <mitchelg555@gmail.com>
2025-12-19 05:14:06 -0500

    Added staging buffers and index buffer

--- 3dRenderingTutorial/lve_model.hpp
+++ 3dRenderingTutorial/lve_model.hpp
@@ -20,7 +20,12 @@ class LveModel {
         getAttributeDescriptions();
     };
 
-    LveModel(LveDevice& device, const std::vector<Vertex>& vertices);
+    struct Builder {
+        std::vector<Vertex> vertices{};
+        std::vector<uint32_t> indices{};
+    };
+
+    LveModel(LveDevice& device, const LveModel::Builder& builder);
     ~LveModel();
     LveModel(const LveModel&) = delete;
     LveModel& operator=(const LveModel&) = delete;
@@ -30,10 +35,16 @@ class LveModel {
 
    private:
     void createVertexBuffers(const std::vector<Vertex>& vertices);
+    void createIndexBuffer(const std::vector<uint32_t>& indices);
 
     LveDevice& lveDevice;
     VkBuffer vertexBuffer;
     VkDeviceMemory vertexBufferMemory;
     uint32_t vertexCount;
+
+    bool hasIndexBuffer = false;
+    VkBuffer indexBuffer;
+    VkDeviceMemory indexBufferMemory;
+    uint32_t indexCount;
 };
 }  // namespace lve
